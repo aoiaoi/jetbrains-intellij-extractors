665b262db9583a5e0fb54df964595bda8d6a79f5
aoiaoi <aoiaoi@users.noreply.github.com>
2018-03-19 15:49:16 +0900

    add extractor for Python

---
+++ Python-Dictionaries-Groovy.py.groovy
@@ -0,0 +1,27 @@
+SEPARATOR = ", "
+QUOTE     = "\'"
+NEWLINE   = System.getProperty("line.separator")
+
+def record(columns, dataRow) {
+    OUT.append("    {")
+    
+    columns.eachWithIndex { column, idx ->
+        def skipQuote = dataRow.value(column).toString().isNumber() || dataRow.value(column) == null
+        def stringValue = FORMATTER.format(dataRow, column)
+        
+        OUT.append(idx == 0 ? "" : "     ")
+                .append(QUOTE).append(column.name()).append(QUOTE)
+                .append(" : ")
+                .append(skipQuote ? "": QUOTE).append(stringValue).append(skipQuote ? "": QUOTE)
+                .append(idx != columns.size() - 1 ? SEPARATOR + NEWLINE : "")
+    }
+    
+    OUT.append(NEWLINE).append("    },").append(NEWLINE)
+}
+
+
+OUT.append("[").append(NEWLINE)
+
+ROWS.each { row -> record(COLUMNS, row) }
+
+OUT.append("]")
